e9fa1e4fdfad404e675f642b8496ecc47fd21e6f
Prueba <erickpr88@gmail.com>
2024-12-11 23:09:30 -0600

    cambios de nombres

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Users Profiles</h3>
+                        <h3 class="card-title">Currículums</h3>
                     </div>
                     <div class="card-body">
                         <table id="user_table" class="table table-bordered table-striped">
